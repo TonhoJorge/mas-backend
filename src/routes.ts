ae1e8094ac46c006bd78852cd563ad202d708214
tonhojorge <ajbfo@hotmail.com>
2021-04-27 10:12:56 -0300

    comitando o que faltou comitar

---
+++ src/routes.ts
@@ -0,0 +1,35 @@
+import {Request, Response, Router} from 'express';
+
+const routes = Router();
+
+routes.get('/user/:id/', (request: Request, response: Response) => {
+    const {id} = request.params;
+    response.json({
+    userId:id
+    })
+})
+
+routes.get('/user/', (request: Request, response: Response) => {
+
+    const {nome, idade} = request.query
+
+    response.json({
+        nome: nome,
+        idade: idade
+    })
+})
+
+routes.post('/user', (request: Request, response: Response) => {
+
+    const {name, email, password} = request.body
+
+    const user = {
+    name,
+    email,
+    password
+    }
+
+    return response.json(user);
+    })
+
+export default routes;
